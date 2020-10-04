da1cb2cd7ff7f1e87ff97f2feee0a4aea779d5f2
Neguro <nassim.ahmedali@gmail.com>
2020-10-04 19:49:53 +0200

    Branche dev : classe Jouet compléter.

--- Ergosum/ClassesMetier/Jouet.cs
+++ Ergosum/ClassesMetier/Jouet.cs
@@ -18,28 +18,25 @@ namespace Ergosum.ClassesMetier
         /// </summary>
         /// <param name="unNumero"></param>
         /// <param name="unLibelle"></param>
-        /// <param name="uneCatégorie"></param>
+        /// <param name="uneCategorie"></param>
         /// <param name="uneTranche"></param>
         public Jouet(int unNumero, string unLibelle, Categorie uneCategorie, TrancheAge uneTranche)
         {
-            // TODO
+            this.numero = unNumero;
+            this.libelle = unLibelle;
+            this.categ = uneCategorie;
+            this.tranche = uneTranche;
+            uneCategorie.AjouterJouet(this); // Ajoute le jouet courant grace a l'opérateur this
         }
-
-        public int Numero { get => numero;  }
-        public string Libelle { get => libelle; }
-        public Categorie Categ { get => categ;  }
-        internal TrancheAge Tranche { get => tranche;  }
          
         /// <summary>
         /// Retourne vrai si le jouet convient à l’âge passé en paramètre.
         /// </summary>
-        /// <param name=""></param>
-        /// <param name=""></param>
-        /// <returns></returns>
-        public bool Convient(int unAge) {
-            // TODO
-            // return à changer
-            return true;
+        /// <param name="unAge"></param>
+        /// <returns>retourne un booléen</returns>
+        public bool Convient(int unAge) 
+        {
+            return ((unAge >= this.tranche.AgeMin) && (unAge <= this.tranche.AgeMax));
         }
 
         /// <summary>
@@ -47,12 +44,15 @@ namespace Ergosum.ClassesMetier
         /// les âges minimum et maximum de la tranche d’âge lui correspondant.
         /// Les informations sont séparées par des points-virgules.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>retourne une chaîne de caractères</returns>
         public string GetInfos()
         {
-            // TODO
-            return "";
-        }        
-
+            return $"Libellé : {this.libelle};\nCatégorie: {this.categ.Libelle};\nAge min: {this.tranche.AgeMin};\nAge max:{this.tranche.AgeMax}";
+        }  
+        
+        public int Numero { get => numero;  }
+        public string Libelle { get => libelle; }
+        public Categorie Categ { get => categ;  }
+        internal TrancheAge Tranche { get => tranche;  }
     }
 }
